77de7c535cc580bfe4e7a3353bf665914479c6a3
steve <stevedbz@hotmail.fr>
2015-09-30 15:57:01 +0200

    tableau deux dimension

--- testfichier.c
+++ testfichier.c
@@ -5,23 +5,29 @@ int main(int argc, char *argv[])
 {
   bool continuer = true;
   int i = 0;
+  int j = 0;
   FILE* fichier = NULL;
   int caractereActuel = 0;
   long pos =0;
   fichier = fopen("jeu.txt", "r");
-  int niveau[46];
+  int niveau[20][20];
   while (continuer)
   {
     caractereActuel = fgetc(fichier); 
-    niveau[i] = caractereActuel;
+    niveau[i][j] = caractereActuel;
     caractereActuel = fgetc(fichier);
-    if (niveau[i] == 10 &  caractereActuel == 10 )
+    if (niveau[i][j] == 10 &  caractereActuel == 10 )
     {
       continuer = false;
     }   
-    fseek(fichier,-1,SEEK_CUR);
-    printf("%c", niveau[i]); 
+    printf("%c", niveau[i][j]); 
     i++;
+    if (niveau[i-1][j] == 10 & caractereActuel != 10)
+    {
+      j++;
+      i=0;
+    }
+    fseek(fichier,-1,SEEK_CUR);
   }
   fclose(fichier);
 
