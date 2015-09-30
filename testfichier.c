25792bd7d1aaf0524adbba433902c985a574af32
Doriane <doriane.lecam@live.com>
2015-09-30 15:43:47 +0200

    test de récup du fichier .txt

--- testfichier.c
+++ testfichier.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(int argc, char *argv[])
 {
+  bool continuer = true;
+  int i = 0;
   FILE* fichier = NULL;
   int caractereActuel = 0;
   long pos =0;
-  fichier = fopen("test.txt", "r");
- 
-  if (fichier != NULL)
+  fichier = fopen("jeu.txt", "r");
+  int niveau[46];
+  while (continuer)
   {
-    do
+    caractereActuel = fgetc(fichier); 
+    niveau[i] = caractereActuel;
+    caractereActuel = fgetc(fichier);
+    if (niveau[i] == 10 &  caractereActuel == 10 )
     {
-      caractereActuel = fgetc(fichier);
-      if (caractereActuel == "")
-      {
-        pos = ftell(fichier);
-        printf("%li", pos);
-      }      
-      printf("%c", caractereActuel);
-      
-    } while (caractereActuel != EOF); 
-    fclose(fichier);
-  } 
+      continuer = false;
+    }   
+    fseek(fichier,-1,SEEK_CUR);
+    printf("%c", niveau[i]); 
+    i++;
+  }
+  fclose(fichier);
+
+
+  //printf("%d%d%d\n",tab[44],tab[45],tab[46]);
   return 0;
 }
